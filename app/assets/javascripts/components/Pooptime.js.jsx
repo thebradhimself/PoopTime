a263831692328cffda0a72dc4636a34d3e059ca4
thebradhimself <Bradley.Hintze@gmail.com>
2016-05-02 18:06:56 -0600

    css tweaks and last use added

--- app/assets/javascripts/components/Pooptime.js.jsx
+++ app/assets/javascripts/components/Pooptime.js.jsx
@@ -8,7 +8,7 @@ class Pooptime extends React.Component{
       this.timerStop = this.timerStop.bind(this);
       this.setWage = this.setWage.bind(this);
       this.getTimeCount = this.getTimeCount.bind(this);
-      this.getMoney = this.getMoney.bind(this);
+      this.format_time = this.format_time.bind(this);
     }
 
     componentWillMount(){
@@ -42,13 +42,16 @@ class Pooptime extends React.Component{
     }
 
     timerStart(){
-      this.setState({start: Date.now(), timer: true});
-      this.timer = setInterval(this.tick, 50);
+      if(this.state.wage){
+        this.setState({start: Date.now(), timer: true});
+        this.timer = setInterval(this.tick, 50);
+      }else
+        alert('Set wage before starting timer, ya dingus');
     }
 
     timerStop(){
-      clearInterval(this.timer);
       this.setState({timer: false, last_money: this.state.money_earned, last_time: this.state.elapsed});
+      clearInterval(this.timer);
     }
 
     setWage(event){
@@ -66,16 +69,19 @@ class Pooptime extends React.Component{
       this.setState({seconds: seconds1, minutes: minutes1, hour: hours1});
     }
 
-    getMoney(){
-
+    format_time(){
+      let elapsed = Math.round(this.state.last_time / 100);
+      let seconds = (elapsed / 10).toFixed(1);
+      let seconds1 = Math.floor(seconds % 60);
+      let minutes = Math.floor(seconds / 60);
+      let minutes1 = Math.floor(minutes % 60);
+      let hours = Math.floor(minutes / 60);
+      let hours1 = Math.floor(hours % 60);
+      return(`${hours1}h ${minutes1}m ${seconds1}s`)
     }
 
     render() {
 
-        // let elapsed = Math.round(this.state.elapsed / 100);
-
-        // This will give a number with one digit after the decimal dot (xx.x):
-        // let seconds = (elapsed / 10).toFixed(1);
         let button = (<button className="btn col s3 ss_button" onClick={this.timerStart}>Start</button>);
         let time = "0h 0m 0s";
         let money = "$0.00";
@@ -84,6 +90,8 @@ class Pooptime extends React.Component{
           time = `${this.state.hour}h ${this.state.minutes}m ${this.state.seconds}s`
           money = `$${this.state.money_earned}`
         }
+        let last_money = this.state.last_money ? `$${this.state.last_money}` : "$0.00";
+        let last_time = this.state.last_time ? `${this.format_time()}` : "0h 0m 0s";
 
         // Although we return an entire <p> element, react will smartly update
         // only the changed parts, which contain the seconds variable.
@@ -101,6 +109,14 @@ class Pooptime extends React.Component{
                     <div className='row'>
                       <input className="hourlyWage col m6" placeholder="Hourly Wage" onKeyUp={this.setWage} />
                       <br />
+                    </div>
+                    <div className='row'>
+                      <div className='chawp_font underline font35 col m6'>Last Use: </div>
+                      <div className='col m3 chawp_font'>
+                        {last_money}
+                        <br />
+                        {last_time}
+                      </div>
                     </div>
                  </div>
                  <div className='col s12 offset-m1 m3'>
